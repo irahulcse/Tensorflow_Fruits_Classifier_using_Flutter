4eda31886f509189466eabbeda4a9d5a2fc40017
Rahul Chandra <1rahulchandra1@gmail.com>
2020-10-07 11:08:56 +0530

    Good First Commit at 2020-10-07-11:08:56

--- git-autopush.sh
+++ git-autopush.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-git pull origin master
+git pull origin main
 git add .
 git commit -m "Good First Commit at `date +%F-%T`"
-git push -f origin master
+git branch -M main
+git push -f origin main
